29c1f6aabe04b06428be87c6c7a7d9fc495e87f9
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2024-11-21 21:31:46 +0200

    Update 775001126.js

--- Data Science/Quizzes/775001126.js
+++ Data Science/Quizzes/775001126.js
@@ -1,6 +1,6 @@
 let question=[
     {
-        numb: 1,
+        numb: 1,type:"Multiple Choice",
         question: "What is the first phase in the data analytics process according to the document?",
         answer: "B) Discovery",
         options: [
@@ -11,7 +11,7 @@ let question=[
         ]
     },
     {
-        numb: 2,
+        numb: 2,type:"Multiple Choice",
         question: "Which of the following tools is suggested for Data Transformation and Cleansing?",
         answer: "B) SQL",
         options: [
@@ -22,7 +22,7 @@ let question=[
         ]
     },
     {
-        numb: 3,
+        numb: 3,type:"Multiple Choice",
         question: "What is the main goal during the 'Model Planning' phase?",
         answer: "C) Determine the appropriate analytic method",
         options: [
@@ -33,7 +33,7 @@ let question=[
         ]
     },
     {
-        numb: 4,
+        numb: 4,type:"Multiple Choice",
         question: "Which of the following is NOT listed as a useful tool for the 'Model Building' phase?",
         answer: "D) Tableau",
         options: [
@@ -44,7 +44,7 @@ let question=[
         ]
     },
     {
-        numb: 5,
+        numb: 5,type:"Multiple Choice",
         question: "What is a key activity during the 'Data Prep' phase?",
         answer: "A) Prepare an analytic sandbox",
         options: [
@@ -55,7 +55,7 @@ let question=[
         ]
     },
     {
-        numb: 6,
+        numb: 6,type:"Multiple Choice",
         question: "Which phase includes tasks such as 'Clean and normalize data'?",
         answer: "C) Data Conditioning",
         options: [
@@ -66,7 +66,7 @@ let question=[
         ]
     },
     {
-        numb: 7,
+        numb: 7,type:"Multiple Choice",
         question: "Which technique is used for the 'Churn Prediction' case study for Yoyodyne Bank?",
         answer: "C) Logistic Regression",
         options: [
@@ -77,7 +77,7 @@ let question=[
         ]
     },
     {
-        numb: 8,
+        numb: 8,type:"Multiple Choice",
         question: "What is the business impact of the churn prediction model for Yoyodyne Bank?",
         answer: "C) Reduction of customer attrition by 25%",
         options: [
@@ -88,7 +88,7 @@ let question=[
         ]
     },
     {
-        numb: 9,
+        numb: 9,type:"Multiple Choice",
         question: "What type of data was used for the Yoyodyne Bank churn prediction model?",
         answer: "B) 5 months of customer account history",
         options: [
@@ -99,7 +99,7 @@ let question=[
         ]
     },
     {
-        numb: 10,
+        numb: 10,type:"Multiple Choice",
         question: "What key factor is a predictor of churn in Yoyodyne Bank's customers?",
         answer: "B) Use of debit card fewer than 5 times per month",
         options: [
@@ -110,7 +110,7 @@ let question=[
         ]
     },
     {
-        numb: 11,
+        numb: 11,type:"Multiple Choice",
         question: "What phase involves 'Selecting methods based on hypotheses'?",
         answer: "D) Model Planning",
         options: [
@@ -121,7 +121,7 @@ let question=[
         ]
     },
     {
-        numb: 12,
+        numb: 12,type:"Multiple Choice",
         question: "During which phase should the analytic team familiarize themselves with data thoroughly?",
         answer: "C) Data Prep",
         options: [
@@ -132,7 +132,7 @@ let question=[
         ]
     },
     {
-        numb: 13,
+        numb: 13,type:"Multiple Choice",
         question: "Which of the following is considered an important role in the final deliverables of a data project?",
         answer: "D) All of the above",
         options: [
@@ -143,7 +143,7 @@ let question=[
         ]
     },
     {
-        numb: 14,
+        numb: 14,type:"Multiple Choice",
         question: "What type of model was selected for the case study's analytic technique?",
         answer: "C) Logistic Regression",
         options: [
@@ -154,7 +154,7 @@ let question=[
         ]
     },
     {
-        numb: 15,
+        numb: 15,type:"Multiple Choice",
         question: "Which tool is suggested for data visualization?",
         answer: "A) R",
         options: [
@@ -165,7 +165,7 @@ let question=[
         ]
     },
     {
-        numb: 16,
+        numb: 16,type:"Multiple Choice",
         question: "What is the key outcome once customers stop using their accounts for gas and groceries in the Yoyodyne case study?",
         answer: "B) They will churn within 60 days",
         options: [
@@ -176,7 +176,7 @@ let question=[
         ]
     },
     {
-        numb: 17,
+        numb: 17,type:"Multiple Choice",
         question: "What is one of the tools mentioned for performing ELT (Extract, Load, Transform)?",
         answer: "A) Hadoop",
         options: [
@@ -187,7 +187,7 @@ let question=[
         ]
     },
     {
-        numb: 18,
+        numb: 18,type:"Multiple Choice",
         question: "Which phase comes after the 'Model Building' phase?",
         answer: "C) Communicate Results",
         options: [
@@ -198,8 +198,8 @@ let question=[
         ]
     },
     {
-        numb: 19,
-        question: "Which tool is recommended for data conditioning?",
+        numb: 19,type:"Multiple Choice",
+        question: "Which tool is recommended for data Quering?",
         answer: "A) SQL",
         options: [
             "A) SQL",
@@ -209,7 +209,7 @@ let question=[
         ]
     },
     {
-        numb: 20,
+        numb: 20,type:"Multiple Choice",
         question: "In the 'Churn Prediction for Yoyodyne Bank' case study, which two types of transactions are significant indicators of customer churn?",
         answer: "A) Gas and grocery purchases",
         options: [
@@ -220,7 +220,7 @@ let question=[
         ]
     },
     {
-        numb: 21,
+        numb: 21,type:"Multiple Choice",
         question: "What is a key task during the 'Communicate Results' phase?",
         answer: "C) Summarize findings for the audience",
         options: [
@@ -231,7 +231,7 @@ let question=[
         ]
     },
     {
-        numb: 22,
+        numb: 22,type:"Multiple Choice",
         question: "Which tool is recommended for collaboration and communication among team members?",
         answer: "D) An online collaboration platform",
         options: [
@@ -242,7 +242,7 @@ let question=[
         ]
     },
     {
-        numb: 23,
+        numb: 23,type:"Multiple Choice",
         question: "What is the main goal of the 'Operationalize' phase?",
         answer: "B) Implement the model in production",
         options: [
@@ -253,7 +253,7 @@ let question=[
         ]
     },
     {
-        numb: 24,
+        numb: 24,type:"Multiple Choice",
         question: "What should be done during 'Model Building' to ensure robustness?",
         answer: "A) Create training, testing, and production data sets",
         options: [
@@ -264,7 +264,7 @@ let question=[
         ]
     },
     {
-        numb: 25,
+        numb: 25,type:"Multiple Choice",
         question: "What is the objective of defining a process to update, retrain, and retire a model?",
         answer: "A) To ensure the model stays relevant over time",
         options: [
