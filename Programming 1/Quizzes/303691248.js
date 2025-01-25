eee1b270151bf65b9ec911aa2e8c66a7ebe83d99
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-01-25 14:07:36 +0200

    Update 303691248.js

--- Programming 1/Quizzes/303691248.js
+++ Programming 1/Quizzes/303691248.js
@@ -1,222 +1,231 @@
-let question= [
+let question=[
     {
         numb: 1,type: "Multiple Choices",
-        question: "1- What is a function in programming?",
-        answer: "D) A self-contained block of statements that perform a specific task",
+        question: "What is an array?",
+        answer: "C) An object that stores many values of the same type",
         options: [
-            "A) A variable that stores data",
-            "B) A command that repeats a block of code",
-            "C) A statement that performs calculations",
-            "D) A self-contained block of statements that perform a specific task"
-        ],
+            "A) A single value of any type",
+            "B) A method that stores multiple values of different types",
+            "C) An object that stores many values of the same type",
+            "D) A variable that can change type"
+        ]
     },
     {
         numb: 2,type: "Multiple Choices",
-        question: "2- What are the two types of functions in programming?",
-        answer: "B) Built-in and User-defined functions",
+        question: "What is an element in the context of arrays?",
+        answer: "B) One value in an array",
         options: [
-            "A) Recursive and Iterative functions",
-            "B) Built-in and User-defined functions",
-            "C) Static and Dynamic functions",
-            "D) Global and Local functions"
-        ],
+            "A) A value stored in a variable",
+            "B) One value in an array",
+            "C) A method to sort arrays",
+            "D) The size of an array"
+        ]
     },
     {
         numb: 3,type: "Multiple Choices",
-        question: "3- What does the return type 'void' signify in a function?",
-        answer: "A) The function does not return any value",
+        question: "How is the index used in arrays?",
+        answer: "B) A 0-based integer to access an element from an array",
         options: [
-            "A) The function does not return any value",
-            "B) The function returns an integer",
-            "C) The function returns a string",
-            "D) The function returns a boolean"
-        ],
+            "A) To store multiple values",
+            "B) A 0-based integer to access an element from an array",
+            "C) To delete elements from an array",
+            "D) To determine the type of elements in an array"
+        ]
     },
     {
         numb: 4,type: "Multiple Choices",
-        question: "4- Where is a global variable declared in a program?",
-        answer: "D) Outside of every function definition",
+        question: "What is the syntax for declaring an array in Java?",
+        answer: "E) Both C and D",
         options: [
-            "A) Inside a specific function",
-            "B) At the end of a function",
-            "C) Within a loop",
-            "D) Outside of every function definition"
-        ],
+            "A) type[] name = type[length];",
+            "B) type name = new type[length];",
+            "C) type[] name = new type[length];",
+            "D) type name[] = new type[length];",
+            "E) Both C and D"
+        ]
     },
     {
         numb: 5,type: "Multiple Choices",
-        question: "5- What is method overloading?",
-        answer: "C) Defining several methods within a class with the same name but different signatures",
+        question: "How do you declare an integer array of length 10?",
+        answer: "B) int[] numbers = new int[10];",
         options: [
-            "A) Creating multiple instances of a class",
-            "B) Using multiple return types in a function",
-            "C) Defining several methods within a class with the same name but different signatures",
-            "D) Calling a function multiple times in a program"
-        ],
+            "A) int[] numbers = new int(10);",
+            "B) int[] numbers = new int[10];",
+            "C) int numbers = new int[10];",
+            "D) int[10] numbers = new int[10];"
+        ]
     },
     {
         numb: 6,type: "Multiple Choices",
-        question: "6- What is the purpose of the parameter list in a function?",
-        answer: "B) To provide input data values to the function",
+        question: "How can the length of an array be dynamically set by the user?",
+        answer: "C) Scanner input = new Scanner(System.in); int n = input.nextInt(); int[] numbers = new int[n];",
         options: [
-            "A) To define the return type of the function",
-            "B) To provide input data values to the function",
-            "C) To declare the function name",
-            "D) To specify the output of the function"
-        ],
+            "A) int n = length.input(); int[] numbers = new int[n];",
+            "B) int n = new Scanner(System.in); int[] numbers = new int[n];",
+            "C) Scanner input = new Scanner(System.in); int n = input.nextInt(); int[] numbers = new int[n];",
+            "D) Scanner input = new Scanner(); int n = input.nextInt(); int[] numbers = new int[n];"
+        ]
     },
     {
         numb: 7,type: "Multiple Choices",
-        question: "7- What is a local variable?",
-        answer: "A) A variable declared within a function or block",
+        question: "Can the length of an array be any integer expression? Give an example.",
+        answer: "E) all the mentioned",
         options: [
-            "A) A variable declared within a function or block",
-            "B) A variable declared outside of all functions",
-            "C) A variable that can be accessed anywhere in the program",
-            "D) A variable that stores a fixed value"
-        ],
+            "A) int x = 2 * 3 + 1; int[] data = new int[x * 5];",
+            "B) int x = 2 + 3 * 1; int[] data = new int[x % 5];",
+            "C) int x = 2 * 3 + 1; int[] data = new int[x % 5 + 2];",
+            "D) int x = 2 * 3 + 1; int[] data = new int[x + 5];",
+            "E) all the mentioned"
+        ]
     },
     {
         numb: 8,type: "Multiple Choices",
-        question: "8- What is a global variable?",
-        answer: "C) A variable declared outside of every function definition",
+        question: "What are the default initial values for different types in arrays?",
+        answer: "C) int: 0, double: 0.0, boolean: false, char: '\\u0000'",
         options: [
-            "A) A variable declared inside a loop",
-            "B) A variable declared inside a specific function",
-            "C) A variable declared outside of every function definition",
-            "D) A variable that cannot be modified"
-        ],
+            "A) int: 0, double: 0.0, boolean: false, char: ' '",
+            "B) int: 0, double: 1.0, boolean: true, char:'0'",
+            "C) int: 0, double: 0.0, boolean: false, char: '\\u0000'",
+            "D) int: -1, double: -1.0, boolean: false, char: '\\0'"
+        ]
     },
     {
         numb: 9,type: "Multiple Choices",
-        question: "9- Which of the following is a correct example of a built-in function in Java?",
-        answer: "B) Math.sqrt(x)",
+        question: "How are array elements accessed and modified?",
+        answer: "B) Access: name[index], Modify: name[index] = value",
         options: [
-            "A) int sum(int a, int b)",
-            "B) Math.sqrt(x)",
-            "C) void display()",
-            "D) double multiply(double a, double b)"
-        ],
+            "A) Access: name.value, Modify: name.value = value",
+            "B) Access: name[index], Modify: name[index] = value",
+            "C) Access: name.value(index), Modify: name.value(index) = value",
+            "D) Access: name(index), Modify: name(index) = value"
+        ]
     },
     {
         numb: 10,type: "Multiple Choices",
-        question: "10- How is a function called in Java?",
-        answer: "D) Using the function name followed by the actual parameter list",
+        question: "Provide an example of modifying the first and fourth elements in an integer array.",
+        answer: "B) int[] numbers = new int[10]; numbers[0] = 27; numbers[4] = -6;",
         options: [
-            "A) By defining the function",
-            "B) By writing the return type first",
-            "C) By declaring a variable",
-            "D) Using the function name followed by the actual parameter list"
-        ],
+            "A) int[] numbers = new int[10]; numbers[1] = 27; numbers[4] = -6;",
+            "B) int[] numbers = new int[10]; numbers[0] = 27; numbers[4] = -6;",
+            "C) int[] numbers = new int[10]; numbers[0] = 27; numbers[3] = -6;",
+            "D) int[] numbers = new int[10]; numbers[1] = 27; numbers[3] = -6;"
+        ]
     },
     {
         numb: 11,type: "Multiple Choices",
-        question: "11- Which keyword is used in Java to indicate that a function does not return a value?",
-        answer: "A) void",
+        question: "How would you declare and initialize a double array of length 5 with specific values?",
+        answer: "C) double[] results = new double[]{0.0, 0.0, 3.4, 0.0, -0.5};",
         options: [
-            "A) void",
-            "B) null",
-            "C) empty",
-            "D) none"
-        ],
+            "A) double[] results = new double[5]; results = {0.0, 0.0, 3.4, 0.0, -0.5};",
+            "B) double[] results = [5]{0.0, 0.0, 3.4, 0.0, -0.5};",
+            "C) double[] results = new double[]{0.0, 0.0, 3.4, 0.0, -0.5};",
+            "D) double results = {0.0, 0.0, 3.4, 0.0, -0.5};"
+        ]
     },
     {
         numb: 12,type: "Multiple Choices",
-        question: "12- What does the scope of a variable determine?",
-        answer: "C) The context within a program in which a variable is valid and can be used",
+        question: "How would you declare a boolean array of length 6 and set the fourth element to true?",
+        answer: "B) boolean[] tests = new boolean[6]; tests[3] = true;",
         options: [
-            "A) The data type of the variable",
-            "B) The memory allocation for the variable",
-            "C) The context within a program in which a variable is valid and can be used",
-            "D) The value of the variable"
-        ],
+            "A) boolean[] tests = new boolean[6]; tests[4] = true;",
+            "B) boolean[] tests = new boolean[6]; tests[3] = true;",
+            "C) boolean[] tests = new boolean[6]; tests[2] = true;",
+            "D) boolean tests = new boolean[6]; tests[3] = true;"
+        ]
     },
     {
         numb: 13,type: "Multiple Choices",
-        question: "13- What happens if a variable is declared inside a function?",
-        answer: "B) It becomes a local variable",
+        question: "How can for loops be used with arrays?",
+        answer: "B) For assigning and accessing values in arrays",
         options: [
-            "A) It becomes a global variable",
-            "B) It becomes a local variable",
-            "C) It is accessible from anywhere in the program",
-            "D) It automatically gets initialized to zero"
-        ],
+            "A) For initializing arrays with values only",
+            "B) For assigning and accessing values in arrays",
+            "C) For creating new arrays",
+            "D) For deleting elements from arrays"
+        ]
     },
     {
         numb: 14,type: "Multiple Choices",
-        question: "14- What is method signature?",
-        answer: "C) The combination of the method name and formal parameter list",
+        question: "What does the length field of an array represent?",
+        answer: "C) The number of elements in the array",
         options: [
-            "A) The return type of the method",
-            "B) The method body",
-            "C) The combination of the method name and formal parameter list",
-            "D) The variables declared in the method"
-        ],
+            "A) The type of elements in the array",
+            "B) The total memory allocated for the array",
+            "C) The number of elements in the array",
+            "D) The maximum value in the array"
+        ]
     },
     {
-        numb: 15,type: "Multiple Choices",
-        question: "15- Which of the following is NOT part of a method signature?",
-        answer: "B) The returned type value",
+        numb: 15,
+        type: "Multiple Choices",
+        question: "How would you initialize an array quickly with known values?",
+        answer: "D) All of the above",
         options: [
-            "A) Method name",
-            "B) The returned type value",
-            "C) Formal parameter list",
-            "D) The order of parameters"
-        ],
+            "A) int[] numbers = {12, 49, -2, 26, 5, 17, -6};",
+            "B) int[] numbers = new int[]{12, 49, -2, 26, 5, 17, -6};",
+            "C) int numbers[] = new int[]{12, 49, -2, 26, 5, 17, -6};",
+            "D) All of the above"
+        ]
     },
     {
-        numb: 16,type: "Multiple Choices",
-        question: "16- In the context of functions, what is overloading?",
-        answer: "D) Defining multiple methods with the same name but different signatures",
+        numb: 16,
+        type: "Multiple Choices",
+        question: "Which of the following data types has the highest precision in Java?",
+        answer: "B) double",
         options: [
-            "A) Using the same method in different classes",
-            "B) Using the same variable name in multiple methods",
-            "C) Declaring the same method multiple times",
-            "D) Defining multiple methods with the same name but different signatures"
-        ],
+            "A) float",
+            "B) double",
+            "C) int",
+            "D) long"
+        ]
     },
     {
-        numb: 17,type: "Multiple Choices",
-        question: "17- What is the output of the following function call: Math.sqrt(25)?",
-        answer: "C) 5.0",
+        numb: 17,
+        type: "Multiple Choices",
+        question: "What is the correct syntax to declare an array in Java?",
+        answer: "d) Both a and c",
         options: [
-            "A) 25",
-            "B) 2.5",
-            "C) 5.0",
-            "D) Error"
-        ],
+            "a) int arr[];",
+            "b) arr int[];",
+            "c) int[] arr;",
+            "d) Both a and c"
+        ]
     },
     {
-        numb: 18,type: "Multiple Choices",
-        question: "18- What type of function is used to sum two integer numbers in the provided examples?",
-        answer: "B) A user-defined function",
+        numb: 18,
+        type: "Multiple Choices",
+        question: "What is the output of the following array operation sequence?",
+        answer: "A) [0, 3, 11, 42, 99, 0, 2, 0]",
         options: [
-            "A) A built-in function",
-            "B) A user-defined function",
-            "C) A recursive function",
-            "D) A void function"
-        ],
+            "A) [0, 3, 11, 42, 99, 0, 2, 0]",
+            "B) [0, 3, 0, 42, 99, 0, 11, 0]",
+            "C) [0, 3, 11, 42, 0, 99, 2, 0]",
+            "D) [0, 3, 42, 11, 99, 0, 2, 0]"
+        ],image:"../../../MDA/1-18 P2.png"
     },
     {
-        numb: 19,type: "Multiple Choices",
-        question: "19- What is required to declare a void function?",
-        answer: "A) Use of the keyword void in the function declaration",
+        numb: 19,
+        type: "Multiple Choices",
+        question: "What is the output of this code snippet using arrays and for loops?",
+        answer: "B) 0 2 4 6 8 10 12 14",
         options: [
-            "A) Use of the keyword void in the function declaration",
-            "B) Declaring a return type",
-            "C) A specific number of parameters",
-            "D) A loop inside the function"
-        ],
+            "A) 2 4 6 8 10 12 14 16",
+            "B) 0 2 4 6 8 10 12 14",
+            "C) 1 3 5 7 9 11 13 15",
+            "D) 0 1 2 3 4 5 6 7"
+        ],image:"../../../MDA/1-19 P2.png"
     },
     {
-        numb: 20,type: "Multiple Choices",
-        question: "20- What is a built-in function?",
-        answer: "B) A function provided by the programming language's standard library",
+        numb: 20,
+        type: "Multiple Choices",
+        question: "What is the output of the following array operation sequence?",
+        answer: "D) false false false true false false",
         options: [
-            "A) A function defined by the user",
-            "B) A function provided by the programming language's standard library",
-            "C) A function that cannot be modified",
-            "D) A function that is only used in large programs"
-        ],
-    },
-];
+            "A) false false false false false false",
+            "B) true false false false false true",
+            "C) true true true true true true true",
+            "D) false false false true false false"
+        ],image:"../../../MDA/1-20 P2.png"
+    }
+
+]
