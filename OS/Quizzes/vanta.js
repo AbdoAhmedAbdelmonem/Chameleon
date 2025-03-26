8f0ef8c37ffd0835958d5dd05143d0b17573911b
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-03-26 19:46:47 +0200

    Add files via upload

---
+++ OS/Quizzes/vanta.js
@@ -0,0 +1,16 @@
+VANTA.NET({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xffeb00,
+  backgroundColor: 0x0,
+  points: 14.00,
+  maxDistance: 24.00,
+  spacing: 16.00,
+  showDots: false
+})
