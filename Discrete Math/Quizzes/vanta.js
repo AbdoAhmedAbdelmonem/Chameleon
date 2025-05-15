7563a4ae1fb2438f2f4fec554c6071de73d7fafb
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-05-15 15:53:21 +0300

    Add files via upload

---
+++ Discrete Math/Quizzes/vanta.js
@@ -0,0 +1,13 @@
+VANTA.CELLS({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    color1: "#731ae5",
+    color2: "#46be0a",
+    size: 0.20,
+    speed: 0.00
+})
