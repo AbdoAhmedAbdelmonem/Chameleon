75fb70e38f4b19835e92ced1e0e848618146637b
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-05-15 15:54:16 +0300

    Update vanta.js

--- Discrete Math/Quizzes/vanta.js
+++ Discrete Math/Quizzes/vanta.js
@@ -6,8 +6,8 @@ VANTA.CELLS({
     minHeight: 200.00,
     minWidth: 200.00,
     scale: 1.00,
-    color1: "#731ae5",
-    color2: "#46be0a",
+    color1: "#ffbd59",
+    color2: "#ffbd59",
     size: 0.20,
     speed: 0.00
 })
