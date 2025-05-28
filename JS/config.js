b0acabc3e72a439afcff1a9c344c2936a57e39d9
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-05-28 18:53:15 +0300

    Add files via upload

---
+++ JS/config.js
@@ -0,0 +1,2 @@
+const apiKey="AIzaSyALQYyTG9yMs9Xd2leIqYgcxybOzFWciY0"
+window.formUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSedOLbgGluxVg3wRhI6wOtEBMgw1jE54zZia-nX1anTfeQPgQ/formResponse";
