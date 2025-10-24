7b849db41e0afa939ffe1da9d1173d9b8814d0c7
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-10-25 01:19:43 +0300

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -1,2 +1,2 @@
-const apiKey="AIzaSyALQYyTG9yMs9Xd2leIqYgcxybOzFWciY0"
+const apiKey="AIzaSyALQYyTG9yMs9Xd2leIqYgcxybOzFWciY"
 window.formUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSedOLbgGluxVg3wRhI6wOtEBMgw1jE54zZia-nX1anTfeQPgQ/formResponse";
